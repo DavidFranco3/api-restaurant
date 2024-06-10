5dcd3d07dd61e21b180863b21bff35f66221d85d
imDemian <cdemian79@gmail.com>
2024-06-10 10:20:26 -0600

    cambios en el schema de comision

--- src/models/comision.js
+++ src/models/comision.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const comision = new Schema({
     nombreComision: { type: String },
     valor: { type: Number },
+    estado: { type: String },
 }, {
     timestamps: true
 });
