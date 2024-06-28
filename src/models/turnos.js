32b1f6d7cac5a882ee26654c67ec4339942ec873
José David Ayala Franco <josedavidayalafranco3@gmail.com>
2024-06-28 14:17:31 -0600

    .

---
+++ src/models/turnos.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose;
+
+// Modelo de la colección turnos
+const turnos = new Schema(
+  {
+    idTurno: { type: String },
+    empleado: { type: String },
+    caja: { type: String },
+    fechaInicio: { type: Date },
+    fechaFinal: { type: Date },
+    observaciones: { type: String },
+    fondoCaja: { type: Number },
+    estado: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("turnos", turnos, "turnos");
---
+++ src/routes/turnos.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const turnos = require("../models/turnos");
+const router = express.Router();
+
+// Registro de turnos
+router.post("/registro", async (req, res) => {
+  const datoTurno = turnos(req.body);
+  await datoTurno
+    .save()
+    .then((data) =>
+      res.status(200).json({
+        mensaje: "Turno registrado",
+        datos: data,
+      })
+    )
+    .catch((error) => res.json({ message: error }));
+});
+
+// Obtener los turnos
+router.get("/listar", async (req, res) => {
+  await turnos
+    .find({ estado: "cerrado" })
+    .sort({ _id: -1 })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Terminar turno
+router.put("/cerrar/:id", async (req, res) => {
+  const { id } = req.params;
+  const { estado, fechaFinal } = req.body;
+  await turnos
+    .updateOne({ _id: id }, { $set: { estado, fechaFinal } })
+    .then((data) =>
+      res.status(200).json({ mensaje: "Estado del ingrediente actualizado" })
+    )
+    .catch((error) => res.json({ message: error }));
+});
