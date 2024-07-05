4c7e912bdb36f7ec8622e6b494012e4a7595b350
imDemian <cdemian79@gmail.com>
2024-07-04 19:47:54 -0600

    cambio prods para poder agregar insumos y controlarlos por ventas

--- src/models/productos.js
+++ src/models/productos.js
@@ -2,17 +2,20 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // modelo de la coleccion productos
-const productos = new Schema({
-  nombre: { type: String },
-  categoria: { type: String },
-  negocio: { type: String },
-  costoProduccion: { type: String },
-  precio: { type: String },
-  imagen: { type: String },
-  ingredientes: { type: Array, default: [] },
-  estado: { type: String },
-}, {
-  timestamps: true
-});
+const productos = new Schema(
+  {
+    nombre: { type: String },
+    categoria: { type: String },
+    negocio: { type: String },
+    costoProduccion: { type: String },
+    precio: { type: String },
+    imagen: { type: String },
+    insumos: { type: Array, default: [] },
+    estado: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("productos", productos, "productos");
