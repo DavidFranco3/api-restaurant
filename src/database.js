1226648dcca99740339eba4b8bfaae835f7e3d11
José David Ayala Franco <josedavidayalafranco3@gmail.com>
2024-08-23 15:39:35 -0600

    .

--- src/database.js
+++ src/database.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 // Credenciales para conexion local a la BD
 // const URI = "mongodb://127.0.0.1/TPV-LA-NENA-PRUEBAS";
 
+// Credenciales para conexion a Mongo Atlas
+const URI = "mongodb+srv://tpv-la-nena:74CDFTgpyW5783eh@cluster0.dsbmu.mongodb.net/restaurantDemo"; // tpv test
 
 // Credenciales para conexion a Mongo Atlas
-const URI = "mongodb+srv://tpv-la-nena:74CDFTgpyW5783eh@cluster0.dsbmu.mongodb.net/restaurant"; // tpv test
+// const URI = "mongodb+srv://tpv-la-nena:74CDFTgpyW5783eh@cluster0.dsbmu.mongodb.net/restaurant"; // tpv test
 
 mongoose.Promise = global.Promise;
 
